5b35db09026b8f0817e827d2927a85b4a6e10fe1
Ibranista <ibranista88@gmail.com>
2023-02-25 08:57:23 +0100

    solve count unique values using a storage object.

--- javascript/multiplePointers.ts
+++ javascript/multiplePointers.ts
@@ -1,7 +1,7 @@
 //accept sorted array of integers and find pair where their sum is zero.
 
 //O(n^2) solution
-function sumZero(nums) {
+function sumZero(nums: any[]) {
   nums = nums.sort();
 
   for (let i = 0; i < nums.length; i++) {
@@ -18,9 +18,8 @@ function sumZero(nums) {
 
 // O(n) solution with out nested loop
 
-function sumZeroRefactored(nums) {
+function sumZeroRefactored(nums: any[]) {
   nums = nums.sort((a, b) => a - b);
-  console.log("sorted: ", nums);
   let left = 0;
   let right = nums.length - 1;
   while (left < right) {
@@ -36,4 +35,26 @@ function sumZeroRefactored(nums) {
   return "nothing found";
 }
 
-console.log(sumZeroRefactored([3, 4, 5, 6, -2, 3, -4]));
+sumZeroRefactored([3, 4, 5, 6, -2, 3, -4]);
+
+// countUniqueValues
+// function that accepts an array and count number of unique values
+
+function countUniqueValues(num: number[]) {
+  let uniqueStorage: { [key: number]: number } = {};
+  for (let values of num) {
+    if (!uniqueStorage[values]) {
+      uniqueStorage[values] = 1;
+    }
+  }
+  let values: string[] = [];
+  for (let uniques in uniqueStorage) {
+    values.push(uniques);
+  }
+  console.log(
+    values.join(","),
+    `so there are ${values.length} number of unique elements.`
+  );
+}
+
+countUniqueValues([2, 3, 3, 5, 5, 5]);
